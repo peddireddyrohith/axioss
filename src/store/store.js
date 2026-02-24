a01657909b84cc9af7f86a7e00511de00d98e915
peddireddyrohith <peddireddyrohith@gmail.com>
2026-02-24 10:22:53 +0530

    redux

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+import appReducer from "./slices/appSlice";
+
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+  },
+});
